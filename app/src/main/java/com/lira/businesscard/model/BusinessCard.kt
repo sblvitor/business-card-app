6d180a6f7d4abc4ea28fef9f630f042e9ecd292d
Vitor <sblvitor@gmail.com>
2022-08-08 16:49:16 -0400

    criacao dos layouts e model

---
+++ app/src/main/java/com/lira/businesscard/model/BusinessCard.kt
@@ -0,0 +1,9 @@
+package com.lira.businesscard.model
+
+data class BusinessCard(
+    val name: String,
+    val mobile: String,
+    val email: String,
+    val company: String,
+    val color: String
+)
--- app/src/main/java/com/lira/businesscard/view/AddBusinessCardActivity.kt
+++ app/src/main/java/com/lira/businesscard/view/AddBusinessCardActivity.kt
@@ -4,23 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.lira.businesscard.R
-import com.lira.businesscard.databinding.ActivityMainBinding
+import com.lira.businesscard.databinding.ActivityAddBusinessCardBinding
 
-class MainActivity : AppCompatActivity() {
+class AddBusinessCardActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityAddBusinessCardBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityAddBusinessCardBinding.inflate(layoutInflater)
         setContentView(binding.root)
         insertListener()
     }
 
     private fun insertListener(){
-        binding.fabAddCard.setOnClickListener {
-            val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
-            startActivity(intent)
+        binding.ibCancel.setOnClickListener {
+            finish()
         }
     }
 }
